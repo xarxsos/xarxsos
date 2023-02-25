f07459a68ec98519e831539a9d3cd01adf37c2ab
Davide Giovara <davide.giovara@gmail.com>
2023-02-25 02:03:25 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// global variable for the player
+var player;
+var playButton = document.getElementById("btnplay");
+
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+  // create the global player from the specific iframe (#video)
+  player = new YT.Player("video", {
+    events: {
+      // call this function when player is ready to use
+      onReady: onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  // bind events
+  playButton.addEventListener("click", function () {
+    player.playVideo();
+  });
+}
+
+function pressPlay() {
+  window.open('https://youtu.be/aLRU9UzvBi4');
+}
+
+// Inject YouTube API script
+var tag = document.createElement("script");
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+window.onload = pressPlay;
